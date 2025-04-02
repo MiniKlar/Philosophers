ae7d4841bf211d8ee9ff31087f3a7ed43c1feb56
MiniKlar <MiniKlar>
2025-04-02 04:39:24 +0200

    Changes to be committed: 	modified:   philo/Makefile 	modified:   philo/includes/philo.h 	new file:   philo/srcs/main.c 	renamed:    philo/main.c -> philo/srcs/utils.c

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,14 +6,18 @@
 /*   By: lomont <lomont@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 06:04:22 by lomont            #+#    #+#             */
-/*   Updated: 2025/03/30 06:39:53 by lomont           ###   ########.fr       */
+/*   Updated: 2025/04/02 04:34:19 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	PHILO_H
 # define PHILO_H
 
-#include "../LIB_C/LIB_C.h"
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
 typedef	struct s_philo
 {
@@ -23,5 +27,7 @@ typedef	struct s_philo
 	int time_to_sleep;
 	char *count_eat_philo;
 }			t_philo;
+
+int	ft_atoi(const char *str);
 
 #endif
