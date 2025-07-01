780e347af413caeeaf35727f7278726ee49adf89
MiniKlar <omontloris@gmail.com>
2025-07-01 15:40:06 +0200

    - Rearrange threads.c file to be compatible with the linked lists.

--- philo/srcs/threads.c
+++ philo/srcs/threads.c
@@ -6,7 +6,7 @@
 /*   By: miniklar <miniklar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 15:42:21 by miniklar          #+#    #+#             */
-/*   Updated: 2025/06/30 18:37:39 by miniklar         ###   ########.fr       */
+/*   Updated: 2025/07/01 15:16:32 by miniklar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,29 +35,61 @@ void	ft_alloc_mutex(t_philo *philo)
 	ft_threads(philo, fork);
 }
 
-int	create_thread(t_philo *philo, pthread_t threads)
+void	give_fork(t_philo *philo, pthread_mutex_t **fork)
 {
-	if (pthread_create(&threads, NULL, &routine, (void *)philo) != 0)
+	int		i;
+	t_philo *tmp;
+
+	tmp = philo;
+	i = 0;
+	while (i < tmp->nb_philosophers)
 	{
-		perror("pthread creation error");
-		exit(EXIT_FAILURE);
+		tmp->left_fork = fork[i];
+		if (tmp->nb_philosophers > 1 && i == 0)
+			tmp->right_fork = fork[tmp->nb_philosophers - 1];
+		else if (tmp->nb_philosophers && i == tmp->nb_philosophers - 1)
+			tmp->right_fork = fork[0];
+		else if (tmp->nb_philosophers == 1)
+			tmp->right_fork = fork[0];
+		else
+			tmp->right_fork = fork[i + 1];
+		i++;
+		if (tmp->next)
+			tmp = tmp->next;
 	}
-	else
-		ft_putstr_fd("Thread successfully created!\n", 1);
+}
+
+int	create_thread(t_philo *philo, pthread_t *threads)
+{
+	int	i;
+
+	i = 0;
+	while (i < philo->nb_philosophers)
+	{
+		if (pthread_create(&threads[i], NULL, &routine, (void *)philo) != 0)
+		{
+			perror("pthread creation error");
+			exit(EXIT_FAILURE);
+		}
+		else
+			ft_putstr_fd("Thread successfully created!\n", 1);
+		if (philo->next)
+			philo = philo->next;
+		i++;
+	}
 	return (0);
 }
 
 int	join_thread(pthread_t *threads, int nb_philo)
 {
-	void	*thread_return;
 	int	i;
 
 	i = 0;
-	thread_return = NULL;
 	while (i < nb_philo)
 	{
 		if (pthread_join(threads[i], NULL) != 0)
 		{
+			perror("join error");
 			ft_putstr_fd("pthread join failed\n", 2);
 			exit(EXIT_FAILURE);
 		}
@@ -80,35 +112,23 @@ void	destroy_mutex(pthread_mutex_t **fork, int nb_philo)
 			perror("mutex destroy error");
 			exit(EXIT_FAILURE);
 		}
+		free(fork[i]);
 		i++;
 	}
+	free(fork);
 }
 
 int	ft_threads(t_philo *philo, pthread_mutex_t **fork)
 {
-	int			i;
 	pthread_t	*threads;
 
-	i = 0;
 	threads = malloc(sizeof(pthread_t) * philo->nb_philosophers);
 	if (!threads)
 		print_error("error allocation threads");
-	while (i < philo->nb_philosophers)
-	{
-		printf("tu boucles \n");
-		philo->left_fork = fork[i];
-		if (philo->nb_philosophers > 1 && i == 0)
-			philo->right_fork = fork[philo->nb_philosophers - 1];
-		else if (philo->nb_philosophers && i == philo->nb_philosophers - 1)
-			philo->right_fork = fork[0];
-		else if (philo->nb_philosophers == 1)
-			philo->right_fork = fork[0];
-		else
-			philo->right_fork = fork[i + 1];
-		create_thread(philo, threads[i]);
-		i++;
-	}
+	give_fork(philo, fork);
+	create_thread(philo, threads);
 	join_thread(threads, philo->nb_philosophers);
 	destroy_mutex(fork, philo->nb_philosophers);
+	free_all(philo, threads);
 	exit(EXIT_SUCCESS);
 }
