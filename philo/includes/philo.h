edbbb26cdaa19d123b0dd551e10398f30e3e8cc7
MiniKlar <omontloris@gmail.com>
2025-07-01 15:36:53 +0200

    •	Added an index to identify threads in the output console. 	•	Created linked lists.

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: miniklar <miniklar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 06:04:22 by lomont            #+#    #+#             */
-/*   Updated: 2025/06/30 18:21:48 by miniklar         ###   ########.fr       */
+/*   Updated: 2025/07/01 15:17:38 by miniklar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,27 +28,30 @@
 
 typedef struct s_philo
 {
-	int	nb_philosophers;
-	int time_to_die;
-	int time_to_eat;
-	int time_to_sleep;
-	int nb_time_philo_needs_to_eat;
+	int				nb_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				nb_time_philo_needs_to_eat;
+	int				index_philo;
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
+	struct	s_philo *next;
 }				t_philo;
 
 void				*routine(void *arg);
 void				check_parsing(int argc, char **argv);
-void				init_philo(t_philo *philo, char **argv, int argc);
 void				ft_alloc_mutex(t_philo *philo);
 
 // Utils
 
+void				free_all(t_philo *philo, pthread_t *threads);
 void				ft_putstr_fd(char *s, int fd);
 void				print_error(char *str);
 bool				ft_isdigit(int c);
 long				ft_atol(const char *str);
 void				free_array(pthread_mutex_t **array);
+t_philo				*fill_philo(t_philo *node, int argc, char **argv);
 
 
 // t_gims ciel;
