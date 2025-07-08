f73e6293c852912b26c45e5d4ba66eb1d3e56b92
MiniKlar <omontloris@gmail.com>
2025-07-08 21:39:57 +0200

    - Modified utils.c

--- philo/srcs/utils.c
+++ philo/srcs/utils.c
@@ -6,24 +6,11 @@
 /*   By: miniklar <miniklar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 02:11:58 by lomont            #+#    #+#             */
-/*   Updated: 2025/06/29 23:09:25 by miniklar         ###   ########.fr       */
+/*   Updated: 2025/07/08 21:29:43 by miniklar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-void	free_array(pthread_mutex_t **array)
-{
-	int	i;
-
-	i = 0;
-	while (array[i] != 0)
-	{
-		free(array[i]);
-		i++;
-	}
-	free(array);
-}
 
 void	ft_putstr_fd(char *s, int fd)
 {
@@ -38,10 +25,57 @@ void	ft_putstr_fd(char *s, int fd)
 	}
 }
 
-void	print_error(char *str)
+void	ft_putchar_fd(char c, int fd)
 {
-	ft_putstr_fd(str, 2);
-	exit(EXIT_FAILURE);
+	if (write(fd, &c, 1) == -1)
+	{
+		perror("write error");
+		return ;
+	}
+}
+
+void	print_message(unsigned long long i, t_philo *p, int code)
+{
+	char	*tmp;
+
+	pthread_mutex_lock(p->dinner->dead_thread);
+	pthread_mutex_lock(p->dinner->m_print);
+	if (!p->dinner->is_dead)
+	{
+		tmp = ft_lltoa(i);
+		ft_putstr_fd(tmp, 1);
+		free(tmp);
+		ft_putchar_fd(' ', 1);
+		tmp = ft_itoa(p->index_philo);
+		ft_putstr_fd(tmp, 1);
+		free(tmp);
+		if (code == 0)
+			ft_putstr_fd(" is thinking\n", 1);
+		else if (code == 1)
+			ft_putstr_fd(" has taken a fork\n", 1);
+		else if (code == 2)
+			ft_putstr_fd(" is eating\n", 1);
+		else if (code == 3)
+			ft_putstr_fd(" is sleeping\n", 1);
+	}
+	pthread_mutex_unlock(p->dinner->m_print);
+	pthread_mutex_unlock(p->dinner->dead_thread);
+}
+
+void	print_death(unsigned long long time, t_philo *p, int i)
+{
+	char	*tmp;
+
+	pthread_mutex_lock(p->dinner->m_print);
+	tmp = ft_lltoa(time);
+	ft_putstr_fd(tmp, 1);
+	free(tmp);
+	ft_putchar_fd(' ', 1);
+	tmp = ft_itoa(i);
+	ft_putstr_fd(tmp, 1);
+	free(tmp);
+	ft_putstr_fd(" died\n", 1);
+	pthread_mutex_unlock(p->dinner->m_print);
 }
 
 bool	ft_isspace(int c)
@@ -86,4 +120,13 @@ long	ft_atol(const char *str)
 	}
 	nb = nb * sign;
 	return (nb);
+}
+
+unsigned long long	get_time(void)
+{
+	struct timeval	time;
+
+	if (gettimeofday(&time, NULL) != 0)
+		ft_putstr_fd("Error gettimeofday", 2);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 }
