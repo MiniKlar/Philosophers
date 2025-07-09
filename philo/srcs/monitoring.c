08b76cf85fa2fdc66cbd78623c2639cc17126e6f
MiniKlar <omontloris@gmail.com>
2025-07-09 22:12:24 +0200

    - Normed monotoring.c file and added function to check how many meals philosophers have eaten to end the simulation.

--- philo/srcs/monitoring.c
+++ philo/srcs/monitoring.c
@@ -6,24 +6,49 @@
 /*   By: miniklar <miniklar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 17:29:41 by miniklar          #+#    #+#             */
-/*   Updated: 2025/07/08 21:18:22 by miniklar         ###   ########.fr       */
+/*   Updated: 2025/07/09 18:12:11 by miniklar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static	void	print_philo_has_finished(t_philo *philo)
+{
+	pthread_mutex_lock(philo->dinner->check);
+	philo->dinner->is_dead = true;
+	pthread_mutex_unlock(philo->dinner->check);
+	ft_putstr_fd("All philosophers are fed!\n", 1);
+}
+
+static void	check_nb_of_philo_meals(t_philo **tmp)
+{
+	while (*tmp)
+	{
+		if ((*tmp)->index_nb_meals == (*tmp)->dinner->nb_philo_needs_to_eat)
+			(*tmp) = (*tmp)->next;
+		else
+			break ;
+	}
+}
+
 unsigned long long	*fill_time(t_philo *philo, unsigned long long *threads)
 {
-	int	i;
 	t_philo	*tmp;
+	int		i;
 
 	i = 0;
 	tmp = philo;
-	threads = malloc(sizeof(unsigned long long) * philo->dinner->nb_philosophers);
+	threads = malloc(sizeof(unsigned long long) * philo->dinner->nb_philo);
 	if (!threads)
 		return (NULL);
-	while (i < philo->dinner->nb_philosophers)
+	if (philo->dinner->index_philo_finish_to_eat
+		== philo->dinner->nb_philo)
+		return (free(threads), print_philo_has_finished(philo), NULL);
+	while (i < philo->dinner->nb_philo)
 	{
+		check_nb_of_philo_meals(&tmp);
+		if (!tmp)
+			return (threads);
 		pthread_mutex_lock(tmp->die_time);
 		threads[i] = tmp->philo_die_time;
 		pthread_mutex_unlock(tmp->die_time);
@@ -34,36 +59,45 @@ unsigned long long	*fill_time(t_philo *philo, unsigned long long *threads)
 	return (threads);
 }
 
-void *monitoring(void *arg)
+static int	check_philo_died(t_philo *philo, unsigned long long *threads)
+{
+	int					i;
+	unsigned long long	time;
+
+	time = get_time();
+	threads = fill_time(philo, threads);
+	if (!threads)
+		return (1);
+	i = 0;
+	while (i < philo->dinner->nb_philo)
+	{
+		if (threads[i] < time)
+		{
+			pthread_mutex_lock(philo->dinner->check);
+			philo->dinner->is_dead = true;
+			pthread_mutex_unlock(philo->dinner->check);
+			print_death(time, philo, i + 1);
+			free(threads);
+			return (1);
+		}
+		else
+			i++;
+	}
+	free(threads);
+	return (0);
+}
+
+void	*monitoring(void *arg)
 {
 	t_philo				*philo;
 	unsigned long long	*threads;
-	unsigned long long	time;
-	int					i;
 
 	philo = (t_philo *)arg;
 	threads = NULL;
 	while (!philo->dinner->is_dead)
 	{
-		time = get_time();
-		threads = fill_time(philo, threads);
-		if (!threads)
-			return (NULL);
-		i = 0;
-		while (i < philo->dinner->nb_philosophers)
-		{
-			if (threads[i] < time)
-			{
-				pthread_mutex_lock(philo->dinner->dead_thread);
-				philo->dinner->is_dead = true;
-				pthread_mutex_unlock(philo->dinner->dead_thread);
-				print_death(time, philo, i + 1);
-				break;
-			}
-			else
-				i++;
-		}
-		free(threads);
+		if (check_philo_died(philo, threads) == 1)
+			break ;
 	}
 	return (NULL);
 }
