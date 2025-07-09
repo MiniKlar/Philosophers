1f8bf05f02c2beecb5f1bf417915aa24b442b5e4
MiniKlar <omontloris@gmail.com>
2025-07-09 22:06:14 +0200

    - Added variable index_nb_meals to count how many times a philo has eaten.

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: miniklar <miniklar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 06:04:22 by lomont            #+#    #+#             */
-/*   Updated: 2025/07/08 21:30:32 by miniklar         ###   ########.fr       */
+/*   Updated: 2025/07/09 18:03:04 by miniklar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,12 @@ typedef struct s_philo
 {
 	unsigned long long	philo_die_time;
 	int					index_philo;
+	int					index_nb_meals;
 	pthread_mutex_t		*left_fork;
 	pthread_mutex_t		*right_fork;
 	pthread_mutex_t		*die_time;
-	struct	s_philo 	*next;
-	struct	s_dinner	*dinner;
+	struct s_philo		*next;
+	struct s_dinner		*dinner;
 }				t_philo;
 
 typedef struct s_dinner
@@ -42,38 +43,43 @@ typedef struct s_dinner
 	int					time_to_die;
 	int					time_to_eat;
 	int					time_to_sleep;
-	int					nb_philosophers;
-	int					nb_time_philo_needs_to_eat;
+	int					nb_philo;
+	int					index_philo_finish_to_eat;
+	int					nb_philo_needs_to_eat;
 	bool				is_dead;
-	pthread_mutex_t		*dead_thread;
+	pthread_mutex_t		*check;
 	pthread_mutex_t		*m_print;
 }				t_dinner;
 
-void				*monitoring(void *arg);
-void				*routine(void *arg);
 bool				check_parsing(int argc, char **argv);
+bool				join_monitor_thread(pthread_t monitor);
+bool				create_monitor_thread(pthread_t *monitor, t_philo *philo);
+int					create_thread(t_philo *philo, pthread_t *threads);
+int					join_thread(pthread_t *threads, int nb_philo);
 int					ft_philo(t_dinner *dinner, t_philo *philo);
+int					fill_philo(t_philo **node, t_dinner *dinner);
+int					fill_dinner(t_dinner **dinner, int argc, char **argv);
+void				*routine(void *arg);
+void				*monitoring(void *arg);
+void				free_philo(t_dinner *dinner, t_philo *philo);
+void				print_message(unsigned long long i, t_philo *p, int code);
+void				print_death(unsigned long long time, t_philo *p, int i);
+void				free_array_mutex(pthread_mutex_t **array);
+void				free_threads_fork(pthread_t *threads,
+						pthread_mutex_t **fork);
 
 // Utils
 
-void				free_philo(t_dinner *dinner, t_philo *philo);
-void				ft_putstr_fd(char *s, int fd);
-void				print_message(unsigned long long i, t_philo *p, int code);
-void				print_death(unsigned long long time, t_philo *p, int i);
-bool				ft_isdigit(int c);
-long				ft_atol(const char *str);
-void				free_array_mutex(pthread_mutex_t **array);
-void				free_threads_fork(pthread_t *threads, pthread_mutex_t **fork);
-int					fill_philo(t_philo **node, t_dinner *dinner);
-int					fill_dinner(t_dinner **dinner, int argc, char **argv);
-unsigned long long	get_time(void);
-char				*ft_lltoa(long long n);
 char				*ft_itoa(int n);
+char				*ft_lltoa(long long n);
+bool				ft_isdigit(int c);
+void				ft_putstr_fd(char *s, int fd);
 void				ft_putchar_fd(char c, int fd);
+long				ft_atol(const char *str);
+unsigned long long	get_time(void);
 
+#endif
 
 // t_gims ciel;
 // t_gims ferrari_vert;
 // t_GIMS DECOUVERT_MAUVAI_REVE;
-
-#endif
